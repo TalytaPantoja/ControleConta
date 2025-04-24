a9686dfaef498e1d812db6bdc4d3a67cf47ec7eb
Talyta Pantoja <talytadaniella16@gmail.com>
2025-04-24 13:47:04 -0300

    Delete Java/src directory

--- Java/src/Praticando.java
+++
@@ -1,9 +0,0 @@
-public class Praticando {
-    public static void main(String[] args) {
-        int contagem = 1;
-        while (contagem <= 10){
-            contagem++;
-            System.out.println(contagem);
-        }
-    }
-}
